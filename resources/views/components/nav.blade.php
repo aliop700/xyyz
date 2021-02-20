f3da4e318527cbebc4de46b0cf5130db38edf7c7
aliop700 <aliop700.as@gmail.com>
2021-02-21 00:06:18 +0300

    .

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -3,8 +3,11 @@
 	     <div class="main-header">
 		 	  @if(auth()->check()):
 			  <div class="carting">
-				 <ul><li><a href="{{route('logout')}}"> Logout</a></li></ul>
+				 <ul><li><a href="#" onclick="document.getElementById('logout-form').submit()"> Logout</a></li></ul>
 			  </div>
+			  <form id="logout-form" action="{{route('logout')}}" method="post" style="display:none" >
+				<input type="submit">
+			  </form>	
 			  @else:
 			  <div class="carting">
 				 <ul><li><a href="{{route('loginPage')}}"> Login</a></li></ul>
@@ -12,7 +15,8 @@
 			  @endif;
 			 <div class="logo">
 				 <h3><a href="{{route('home')}}"><img width="200px" src="images/logo.png"/></a></h3>
-			  </div>			  
+			  </div>
+		  
 			 <div class="box_1">				 
 				 <a href="#"><h3>Basket:  (<span id="simpleCart_quantity" class="simpleCart_quantity"></span> الاصناف)<img src="images/cart.png" alt=""/></h3></a>
 				 <p><a href="javascript:;" class="simpleCart_empty"> empty basket</a></p>
