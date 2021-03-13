a7b0f6ad8bac4b292385b1ac9e2a7edb18eeab37
AbdTarboush <abodtarbosh@gmail.com>
2021-03-13 20:26:22 +0200

    once delete cars before product && footer logo route

--- resources/views/components/admin_footer.blade.php
+++ resources/views/components/admin_footer.blade.php
@@ -1,6 +1,6 @@
 <div class="fotter-logo">
 	 <div class="container">
-	 <div class="ftr-logo"><h3><a href="index.html"><img src="/images/white_logo_2.png" width="150px" alt=""></a></h3></div>
+	 <div class="ftr-logo"><h3><a href="{{route('home')}}"><img src="/images/white_logo_2.png" width="150px" alt=""></a></h3></div>
 	 <div class="ftr-info">
 	 
 	</div>
